75cc98e5f9bddf98e6b28f77d7ec2515d57b6d79
Samuel Guerrero <itougn15@gmail.com>
2023-06-08 00:19:11 -0600

    Added landing mobile version

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-'use client'
+"use client";
 import { Button } from "@/components/Button";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
 
   return (
-    <main className="flex min-h-screen bg-white">
-      <section className="w-1/2 bg-black flex">
-        <div className="mx-auto my-auto">
+    <main className="flex flex-col md:flex-row md:min-h-screen h-screen bg-white">
+      <section className="md:w-1/2 md:h-screen w-full h-1/2 bg-black flex">
+        <div className="mx-auto my-auto flex justify-center">
           <Image
-            className="rounded-md shadow-yellow-600 shadow-2xl"
+            className="rounded-md md:w-[600px] w-[calc(100%-2rem)] shadow-yellow-600 shadow-2xl"
             src={"/AlEd.png"}
             alt="AlEd Logo"
             width={600}
@@ -19,19 +19,19 @@ export default function Home() {
           />
         </div>
       </section>
-      <section className="w-1/2 flex bg-slate-100">
-        <div className="mx-auto my-auto flex items-center">
-          <div className="w-3/4 mx-auto flex flex-col text-center">
-            <h1 className="text-black font-akshar font-normal text-7xl mb-24">
+      <section className="md:w-1/2 md:h-screen w-full h-1/2 flex bg-slate-100">
+         <div className="mx-auto my-auto flex items-center">
+          <div className="md:w-3/4 w-10/12 mx-auto flex flex-col text-center">
+            <h1 className="text-black font-akshar font-normal text-3xl md:text-7xl md:mb-24 mb-5">
               AlEd es una empresa con más de 5000 empleados en toda
               Latinoamérica
             </h1>
-            <h1 className="text-black font-akshar font-normal text-3xl mb-10">
+            <h1 className="text-black font-akshar font-normal text-xl md:text-3xl mb-10">
               Gestiona los perfiles de los usuarios de AlEd
             </h1>
             <Button onClick={() => router.push("/users")} variant="principal">Ir</Button>
           </div>
-        </div>
+        </div> 
       </section>
     </main>
   );
