b89cf534e108deb6ff2f0cad35e846662120ef3a
Samuel Guerrero <itougn15@gmail.com>
2023-06-08 14:58:55 -0600

    More changes to users page

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/Button";
 import { NavBar } from "@/components/NavBar";
 import { UserCard } from "@/components/UserCard";
 import React from "react";
@@ -8,12 +9,26 @@ const data = [
     name: "Emma Proulx",
     profession: "Visual Designer",
     urlImage: "https://i.postimg.cc/25nkBqxG/Emma.jpg",
+    age: 28,
+    nationality: "Estadounidense",
+    height: "1.68",
+    telephoneNumber: "59-6823-2351",
+    facebook: "Emma Proulx",
+    twitter: "@menitrust",
+    github: "Emma Proulx",
   },
   {
     id: "2",
     name: "Kevin Parker",
     profession: "Singer",
     urlImage: "https://i.postimg.cc/8Cc7X7SQ/Kevin.webp",
+    age: 35,
+    nationality: "Australiano",
+    height: "1.74",
+    telephoneNumber: "47-2614-7162",
+    facebook: "Kevin Parker",
+    twitter: "@tameimpala",
+    github: "Kevin Parker",
   },
 ];
 
@@ -21,12 +36,20 @@ export default function Home() {
   return (
     <div className="flex min-h-screen bg-slate-100">
       <NavBar toolSelected="Aled" />
-      <section className="container flex flex-col items-center pt-6 pb-6 mt-24 mx-auto bg-slate-200 rounded-md shadow-sm shadow-black space-y-8">
-        {data.map((element) => (
+      <section className="container flex flex-col items-center pt-6 pb-6 mt-24 mx-auto rounded shadow-sm shadow-black space-y-8">
+        <Button variant="secondary">Agregar</Button>
+        {data.map((professional) => (
           <UserCard
-            name={element.name}
-            profession={element.profession}
-            urlImage={element.urlImage}
+            name={professional.name}
+            profession={professional.profession}
+            urlImage={professional.urlImage}
+            age={professional.age}
+            nationality={professional.nationality}
+            height={professional.height}
+            telephoneNumber={professional.telephoneNumber}
+            facebook={professional.facebook}
+            twitter={professional.twitter}
+            github={professional.github}
           />
         ))}
       </section>
