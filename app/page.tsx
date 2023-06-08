8eae257047e613ce7f9bdff2c44b0320b93213d4
Samuel Guerrero <itougn15@gmail.com>
2023-06-08 09:11:37 -0600

    Added Landing Page tablet UI version

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <main className="flex flex-col md:flex-row md:min-h-screen h-screen bg-white">
-      <section className="md:w-1/2 md:h-screen w-full h-1/2 bg-black flex">
+    <main className="flex flex-col lg:flex-row lg:min-h-screen h-screen bg-white">
+      <section className="lg:w-1/2 lg:h-screen w-full h-1/2 bg-black flex">
         <div className="mx-auto my-auto flex justify-center">
           <Image
-            className="rounded-md md:w-[600px] w-[calc(100%-2rem)] shadow-yellow-600 shadow-2xl"
+            className="rounded-md lg:w-[600px] sm:w-[400px] w-[calc(100%-2rem)] shadow-yellow-600 shadow-2xl"
             src={"/AlEd.png"}
             alt="AlEd Logo"
             width={600}
@@ -19,14 +19,14 @@ export default function Home() {
           />
         </div>
       </section>
-      <section className="md:w-1/2 md:h-screen w-full h-1/2 flex bg-slate-100">
+      <section className="lg:w-1/2 lg:h-screen w-full h-1/2 flex bg-slate-100">
          <div className="mx-auto my-auto flex items-center">
-          <div className="md:w-3/4 w-10/12 mx-auto flex flex-col text-center">
-            <h1 className="text-black font-akshar font-normal text-3xl md:text-7xl md:mb-24 mb-5">
+          <div className="lg:w-3/4 w-10/12 mx-auto flex flex-col text-center">
+            <h1 className="text-black font-akshar font-normal text-3xl sm:text-5xl lg:text-7xl lg:mb-24 sm:mb-10 mb-5">
               AlEd es una empresa con más de 5000 empleados en toda
               Latinoamérica
             </h1>
-            <h1 className="text-black font-akshar font-normal text-xl md:text-3xl mb-10">
+            <h1 className="text-black font-akshar font-normal text-xl sm:text-3xl lg:text-3xl mb-10">
               Gestiona los perfiles de los usuarios de AlEd
             </h1>
             <Button onClick={() => router.push("/users")} variant="principal">Ir</Button>
