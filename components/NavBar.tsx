3aa1f77a71f74bec753badb42a1f6ba3f43e016c
Samuel Guerrero <itougn15@gmail.com>
2023-06-09 22:31:28 -0600

    Deleted innecesary prop

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,18 +2,14 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
-type NavBarProps = {
-  toolSelected: string;
-};
-
-export const NavBar = (props: NavBarProps) => {
-  const { toolSelected } = props;
+export const NavBar = () => {
   const router = useRouter()
 
   return (
     <nav className="w-full h-20 fixed z-50 top-0 flex bg-black justify-center items-center shadow-md shadow-yellow-600">
         <Image
           className="hover:cursor-pointer"
+          priority={true}
           onClick={() => router.push("/")}
           src={"/AlEdLogo.png"}
           width={220}
